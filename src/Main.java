4c3f4c81241f86dde470cc3df55227e663b2ea88
brittbec <brittany@3fourteen.co>
2018-01-31 14:25:23 -0600

    setting up Main Class

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.util.Scanner; 
 
-/**
- *
- * @author brittanybeckett-harrison
- */
 public class Main {
     
+    private static Scanner keyboard = new Scanner(system.in);
+    
+    public static void Main(String args[]) {
+    
+    
+    System.out.println("What is the name of the ingredient you want to add? ");
+    keyboard.nextLine();
+    
+    
+    
+    }
 }
